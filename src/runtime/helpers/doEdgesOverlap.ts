f3d0f0f014f07d54acc6aa0094421c3c467ff616
Alan North <alanscodelog@gmail.com>
2026-05-09 23:30:32 -0300

    v0feat: made doEdgesOverlap more flexible with inclusive option

--- src/runtime/helpers/doEdgesOverlap.ts
+++ src/runtime/helpers/doEdgesOverlap.ts
@@ -4,22 +4,28 @@ import { isEdgeParallel } from "./isEdgeParallel.js"
 
 import type { Edge, Orientation } from "../types/index.js"
 
-export function doEdgesOverlap(edgeA: Edge, edgeB: Edge, dir?: Orientation): boolean {
+export function doEdgesOverlap(
+	edgeA: Edge,
+	edgeB: Edge,
+	dir?: Orientation,
+	/** If you pass inclusive true it will also match edges that start where the other edge ends or vice versa. */
+	inclusive: boolean = false
+): boolean {
 	dir ??= getEdgeOrientation(edgeA)
 	if (!isEdgeParallel(edgeA, edgeB)) return false
 	if (dir === "horizontal") {
 		if (edgeA.startY !== edgeB.startY) return false
 		const leftMost = edgeA.startX <= edgeB.startX ? edgeA : edgeB
 		const rightMost = edgeB === leftMost ? edgeA : edgeB
-		const startOverlaps = inRange(leftMost.endX, rightMost.startX, rightMost.endX)
-		const endOverlaps = inRange(rightMost.startX, leftMost.startX, leftMost.endX)
+		const startOverlaps = inRange(leftMost.endX, rightMost.startX, rightMost.endX, inclusive)
+		const endOverlaps = inRange(rightMost.startX, leftMost.startX, leftMost.endX, inclusive)
 		return startOverlaps || endOverlaps
 	} else {
 		if (edgeA.startX !== edgeB.startX) return false
 		const topMost = edgeA.startY <= edgeB.startY ? edgeA : edgeB
 		const bottomMost = edgeB === topMost ? edgeA : edgeB
-		const startOverlaps = inRange(topMost.endY, bottomMost.startY, bottomMost.endY)
-		const endOverlaps = inRange(bottomMost.startY, topMost.startY, topMost.endY)
+		const startOverlaps = inRange(topMost.endY, bottomMost.startY, bottomMost.endY, inclusive)
+		const endOverlaps = inRange(bottomMost.startY, topMost.startY, topMost.endY, inclusive)
 		return startOverlaps || endOverlaps
 	}
 }
