495e8e4cd805106018fd72f54dab182269f2f065
Alan North <alanscodelog@gmail.com>
2026-07-06 16:55:33 -0300

    v0fix: fixed missing decos from refactor

--- src/module.ts
+++ src/module.ts
@@ -38,7 +38,7 @@ export default defineNuxtModule<ModuleOptions>({
 			getContents: () => [
 				`@source "${resolve("runtime/components")}";`,
 				// the drag actions handlers and utils they use add some styles
-				`@source "${resolve("runtime/drag")}";`,
+				`@source "${resolve("runtime/move")}";`,
 				`@source "${resolve("runtime/utils/createSplitDecoShapes")}";`
 			].join("\n")
 		})
